75ea83ad673e40856686ae24af7fabb8b84170bd
Nasina-Srilahari <nasinasrilahariias@gmail.com>
2023-08-05 17:20:49 +0530

    first

---
+++ FireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAD_Jo0LBQYNcPd8X4W-a4qIr5ChWhmBdA",
+  authDomain: "formcrud-656f5.firebaseapp.com",
+  databaseURL: "https://formcrud-656f5-default-rtdb.firebaseio.com",
+  projectId: "formcrud-656f5",
+  storageBucket: "formcrud-656f5.appspot.com",
+  messagingSenderId: "337937727505",
+  appId: "1:337937727505:web:4af0b3938f0b7dec8ff1b0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
